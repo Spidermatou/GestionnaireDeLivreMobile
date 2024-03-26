9d2648b910465a1697f86a9d9ab7c80f5c6725e1
FERNANDES Samuel <samuel.fernandes@etu.unistra.fr>
2024-03-26 09:49:16 +0100

    create clickable

--- ABC/app/src/main/java/com/example/abc/AuthorsListFragment.java
+++ ABC/app/src/main/java/com/example/abc/AuthorsListFragment.java
@@ -7,13 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AuthorsListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AuthorsListFragment extends Fragment {
+public class AuthorsListFragment extends Fragment implements HasClickableItem {
 
     public AuthorsListFragment() {
         // Required empty public constructor
@@ -39,5 +40,11 @@ public class AuthorsListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_authors_list, container, false);
+    }
+
+    @Override
+    public void onClick(int index) { // TODO - is just a test yet
+        Toast toast = Toast.makeText(getActivity(),"CLICKED",Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
---
+++ ABC/app/src/main/java/com/example/abc/HasClickableItem.java
@@ -0,0 +1,5 @@
+package com.example.abc;
+
+public interface HasClickableItem {
+    void onClick(int index);
+}
